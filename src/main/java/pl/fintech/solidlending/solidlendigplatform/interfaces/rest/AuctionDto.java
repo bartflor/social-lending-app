b3e504a17b73c0f6612d7c768a49b8af919abe18
Bartosz Floryan <bartflor@gmail.com>
2020-11-16 08:42:38 +0100

    add date serialization setter

--- src/main/java/pl/fintech/solidlending/solidlendigplatform/interfaces/rest/AuctionDto.java
+++ src/main/java/pl/fintech/solidlending/solidlendigplatform/interfaces/rest/AuctionDto.java
@@ -1,30 +1,44 @@
 package pl.fintech.solidlending.solidlendigplatform.interfaces.rest;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import pl.fintech.solidlending.solidlendigplatform.domain.auction.Auction;
 import pl.fintech.solidlending.solidlendigplatform.domain.auction.AuctionLoanParams;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.List;
 import java.util.stream.Collectors;
-
-@Value
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AuctionDto {
-    Long id;
-    String borrower;
-    double borrowerRating;
-    double amount;
-    double rate;
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Europe/Warsaw")
-    Instant loanStartDate;
-    int loanDuration;//in months
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Europe/Warsaw")
-    Instant auctionStartDate;
-    int auctionDuration;//in days
-    String status;
-    List<OfferDto> offers;
+    private Long id;
+    private String borrower;
+    private double borrowerRating;
+    private double amount;
+    private double rate;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
+    private Instant loanStartDate;
+    private int loanDuration;//in months
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
+    private Instant auctionStartDate;
+    private int auctionDuration;//in days
+    private String status;
+    private List<OfferDto> offers;
+    
+    @JsonSetter
+    private void setLoanStartDate(String time) {
+        this.loanStartDate = Instant.parse(time.concat("T00:00:00Z"));
+    }
   
     public static AuctionDto fromAuction(Auction auction) {
         AuctionLoanParams params = auction.getAuctionLoanParams();
